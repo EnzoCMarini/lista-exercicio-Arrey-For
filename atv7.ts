a718f67abb7f4b093093bdde88fcf476021cbc5b
EnzoCMarini <enzocassao@gmail.com>
2025-06-03 09:17:43 -0300

    removendo uma parte inutil

--- atv7.ts
+++ atv7.ts
@@ -6,7 +6,6 @@ usuário. utilize laço de repetição for.*/
 let teclado = require(`prompt-sync`)();
 
 let n: number = parseInt(teclado(`Digite o seu número: `));
-let save = n;
 
 for (let multi = n - 1; multi > 0; multi--) {
     n = n * multi;
